45a02f0e51f4183919b9e91a7d601eb275b675d5
KravAD <dkrav@myuax.com>
2024-04-07 16:54:04 +0200

    commit 4
    Extensión de JFrame para la implementación de interfaz gráfica.

--- GestionPublicaciones/src/main/java/CreacionAlmacen/TextEditor.java
+++ GestionPublicaciones/src/main/java/CreacionAlmacen/TextEditor.java
@@ -1,12 +1,21 @@
 package CreacionAlmacen;
-
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
-public class TextEditor{
+public class TextEditor extends JFrame {
     private JTextArea textArea;
     private JButton saveButton;
     private JList<String> fileList;
     private DefaultListModel<String> listModel;
+
+
 
 }
