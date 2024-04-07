b1dcb3c033b8c66b5d8d5ecde3c9c7285d911d51
KravAD <dkrav@myuax.com>
2024-04-07 21:38:23 +0200

    commit 29
    Crea un lista de contactos , puede añadirlos, borrarlos y mostrarlos.

--- GestionPublicaciones/src/main/java/CreacionAlmacen/Gestor.java
+++ GestionPublicaciones/src/main/java/CreacionAlmacen/Gestor.java
@@ -2,4 +2,21 @@ package CreacionAlmacen;
 import java.util.ArrayList;
 import java.util.List;
 public class Gestor {
+    private List<Contact> contacts;
+
+    public ContactManager() {
+        contacts = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void removeContact(Contact contact) {
+        contacts.remove(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
 }
