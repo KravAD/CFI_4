1488da65080d0cbee1bc4bdb67c146cf2c2435b3
KravAD <dkrav@myuax.com>
2024-04-07 21:28:29 +0200

    commit 25
    Atributos de nombre, email y número de teléfono y el constructor.

--- GestionPublicaciones/src/main/java/CreacionAlmacen/Contact.java
+++ GestionPublicaciones/src/main/java/CreacionAlmacen/Contact.java
@@ -1,4 +1,13 @@
 package CreacionAlmacen;
 
 public class Contact {
+    private String name;
+    private String email;
+    private String number;
+
+    public Contact(String name, String email, String number) {
+        this.name = name;
+        this.email = email;
+        this.number = number;
+    }
 }
