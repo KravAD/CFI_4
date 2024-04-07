46af8a179f1921ead660764d54cb34a1c45c9be4
KravAD <dkrav@myuax.com>
2024-04-07 20:17:01 +0200

    commit 20
    Clase FileAnalyzer con el método analyze. Si el archivo tiene nombre hace la cuenta de líneas.

--- GestionPublicaciones/src/main/java/Analizar/FileAnalyzer.java
+++ GestionPublicaciones/src/main/java/Analizar/FileAnalyzer.java
@@ -1,5 +1,18 @@
 package Analizar;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 public class FileAnalyzer {
+    public void analyze(String fileName) {
+        if (fileName != null) {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get(fileName));
+                System.out.println("The file " + fileName + " has " + lines.size() + " lines.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
