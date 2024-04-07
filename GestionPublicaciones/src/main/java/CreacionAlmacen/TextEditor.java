43db0221373ea0e8c225c03cbbb691ba504483d9
KravAD <dkrav@myuax.com>
2024-04-07 16:43:58 +0200

    commit 3
    Atributos para la clase editor de texto. Añade área de texto, botón de guardar, lista de archivos y modelo de lista.

---
+++ GestionPublicaciones/src/main/java/CreacionAlmacen/TextEditor.java
@@ -0,0 +1,12 @@
+package CreacionAlmacen;
+
+
+import javax.swing.*;
+
+public class TextEditor{
+    private JTextArea textArea;
+    private JButton saveButton;
+    private JList<String> fileList;
+    private DefaultListModel<String> listModel;
+
+}
