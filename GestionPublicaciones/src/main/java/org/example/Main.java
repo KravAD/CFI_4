eba314d71a855d9da36f20845a81324be538f2be
KravAD <dkrav@myuax.com>
2024-04-07 17:27:29 +0200

    commit 10
     Se implementa el swing para la interfaz de usuario para garantizar el rendimiento y la estabilidad.  Dentro de este se encuentra una instancia de la clase TextEditor y un setVisible que hace la interfaz de usuario visible.

--- GestionPublicaciones/src/main/java/org/example/Main.java
+++ GestionPublicaciones/src/main/java/org/example/Main.java
@@ -1,8 +1,14 @@
 package org.example;
 import CreacionAlmacen.*;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TextEditor textEditor = new TextEditor();
+            textEditor.setVisible(true);
+        });
 
     }
 }
